397457d006f931aef9074741fff18a63815bbf0a
Fábio Maiorano <fsmaiorano@gmail.com>
2024-01-20 14:55:02 +0000

    Update dependencies and add new features

---
+++ pizzashop-web/src/pages/app/orders/order-table-row.tsx
@@ -0,0 +1,45 @@
+import { ArrowRight, Search, X } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import { TableCell, TableRow } from '@/components/ui/table'
+
+export interface OrderTableRowProps {
+  i: number
+}
+
+export function OrderTableRow() {
+  return (
+    <TableRow>
+      <TableCell>
+        <Button variant="outline" size="xs">
+          <Search className="h-3 w-3" />
+          <span className="sr-only">Order detail</span>
+        </Button>
+      </TableCell>
+      <TableCell className="font-mono text-xs font-medium">
+        dds767d9sa789d7sa
+      </TableCell>
+      <TableCell className="text-muted-foreground">2 hours ago</TableCell>
+      <TableCell>
+        <div className="flex items-center gap-2">
+          <span className="h-2 w-2 rounded-full bg-slate-400" />
+          <span className="font-medium text-muted-foreground">Pending</span>
+        </div>
+      </TableCell>
+      <TableCell className="font-medium">Client full name</TableCell>
+      <TableCell className="font-medium">149,00</TableCell>
+      <TableCell>
+        <Button variant="outline" size="xs">
+          <ArrowRight className="mr-2 h-3 w-3" />
+          Approve
+        </Button>
+      </TableCell>
+      <TableCell>
+        <Button variant="ghost" size="xs">
+          <X className="mr-2 h-3 w-3" />
+          Cancel
+        </Button>
+      </TableCell>
+    </TableRow>
+  )
+}
