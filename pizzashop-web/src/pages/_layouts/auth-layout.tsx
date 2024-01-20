a74da6a7acc52bd28a781a034e7953cf277c3729
Fábio Maiorano <fsmaiorano@gmail.com>
2024-01-20 12:17:18 +0000

    Add Toaster component and update dependencies

--- pizzashop-web/src/pages/_layouts/auth-layout.tsx
+++ pizzashop-web/src/pages/_layouts/auth-layout.tsx
@@ -1,10 +1,19 @@
+import { Pizza } from 'lucide-react'
 import { Outlet } from 'react-router-dom'
 
 export function AuthLayout() {
   return (
-    <div>
-      <h1>Auth Layout</h1>
-      <div>
+    <div className="grid min-h-screen grid-cols-2">
+      <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
+        <div className="flex items-center gap-3 text-lg font-medium text-foreground">
+          <Pizza className="h-5 w-5" />
+          <span>Pizza Shop</span>
+        </div>
+        <footer className="text-sm">
+          <p>&copy; 2021 Pizza Shop. All rights reserved.</p>
+        </footer>
+      </div>
+      <div className='flex flex-col justify-center items-center'>
         <Outlet />
       </div>
     </div>
--- pizzashop-web/src/pages/auth/sign-in.tsx
+++ pizzashop-web/src/pages/auth/sign-in.tsx
@@ -1,10 +1,57 @@
 import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+import { z } from 'zod'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
+const signInForm = z.object({
+  email: z.string().email(),
+})
+
+type SignInForm = z.infer<typeof signInForm>
 
 export function SignIn() {
+  const { handleSubmit, register, formState } = useForm<SignInForm>()
+
+  async function handleSignIn(data: SignInForm) {
+    try {
+      console.log(data)
+      await new Promise((resolve) => setTimeout(resolve, 1000))
+      toast.success('We send you an email with a link to sign in')
+    } catch {
+      toast.error('Something went wrong')
+    }
+  }
+
   return (
     <>
-      <Helmet title='Sign-in'/>
-      <h1>Sign In</h1>
+      <Helmet title="Sign-in" />
+      <div className="p-8">
+        <div className="flex w-[320px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">Dashboard</h1>
+            <p className="text-sm text-muted-foreground">
+              Sign in to your account
+            </p>
+          </div>
+          <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" type="email" {...register('email')} />
+            </div>
+            <Button
+              disabled={formState.isSubmitting}
+              type="submit"
+              className="w-full"
+            >
+              Sign in
+            </Button>
+          </form>
+        </div>
+      </div>
     </>
   )
 }
