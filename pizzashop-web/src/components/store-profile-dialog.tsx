c3f4377ce9816aabb82587f529a3c6b9d9922b65
Fábio Maiorano <fsmaiorano@gmail.com>
2024-01-21 14:36:23 +0000

    Update store profile description to be nullable and add function to update managed restaurant cache

--- pizzashop-web/src/components/store-profile-dialog.tsx
+++ pizzashop-web/src/components/store-profile-dialog.tsx
@@ -28,7 +28,7 @@ import { Textarea } from './ui/textarea'
 
 const storeProfileSchema = z.object({
   name: z.string().min(1),
-  description: z.string(),
+  description: z.string().nullable(),
 })
 
 type StoreProfileSchema = z.infer<typeof storeProfileSchema>
@@ -54,20 +54,38 @@ export function StoreProfileDialog() {
     resolver: zodResolver(storeProfileSchema),
   })
 
+  function updateManagedRestaurantCache({
+    name,
+    description,
+  }: StoreProfileSchema) {
+    const cached = queryClient.getQueryData<GetManagedRestaurantResponse>([
+      'managed-restaurant',
+    ])
+    if (cached) {
+      queryClient.setQueryData<GetManagedRestaurantResponse>(
+        ['managed-restaurant'],
+        {
+          ...cached,
+          name,
+          description,
+        },
+      )
+    }
+
+    return { cached }
+  }
+
   const { mutateAsync: updateProfileFn } = useMutation({
     mutationFn: updateProfile,
-    onSuccess: (_, { name, description }) => {
-      const cached = queryClient.getQueryData<GetManagedRestaurantResponse>([
-        'managed-restaurant',
-      ])
-      if (cached) {
-        queryClient.setQueryData<GetManagedRestaurantResponse>(
+    onMutate: async ({ name, description }) => {
+      const { cached } = updateManagedRestaurantCache({ name, description })
+      return { previousProfile: cached }
+    },
+    onError: (_, __, context) => {
+      if (context?.previousProfile) {
+        queryClient.setQueryData(
           ['managed-restaurant'],
-          {
-            ...cached,
-            name,
-            description,
-          },
+          context.previousProfile,
         )
       }
     },
