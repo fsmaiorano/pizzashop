dfe88a42eef73dd89b3e82dae906e5e6c70ec2c1
Fábio Maiorano <fsmaiorano@gmail.com>
2024-01-23 16:10:49 +0000

    Refactor popular products chart and update API endpoint

--- pizzashop-web/src/pages/app/dashboard/popular-products-chart.tsx
+++ pizzashop-web/src/pages/app/dashboard/popular-products-chart.tsx
@@ -1,16 +1,18 @@
+import { useQuery } from '@tanstack/react-query'
 import { BarChart } from 'lucide-react'
 import { Cell, Pie, PieChart, ResponsiveContainer } from 'recharts'
 import colors from 'tailwindcss/colors'
 
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { getPopularProducts } from '@/services/get-popular-products'
 
-const data = [
-  { product: 'Pepperoni', amount: 40 },
-  { product: 'Mussarela', amount: 20 },
-  { product: 'Marguerita', amount: 22 },
-  { product: '4 Formaggi', amount: 35 },
-  { product: 'Tuna', amount: 32 },
-]
+// const data = [
+//   { product: 'Pepperoni', amount: 40 },
+//   { product: 'Mussarela', amount: 20 },
+//   { product: 'Marguerita', amount: 22 },
+//   { product: '4 Formaggi', amount: 35 },
+//   { product: 'Tuna', amount: 32 },
+// ]
 
 const COLORS = [
   colors.sky['500'],
@@ -21,6 +23,11 @@ const COLORS = [
 ]
 
 export function PopularProductsChart() {
+  const { data: popularProducts } = useQuery({
+    queryKey: ['metrics', 'popular-products'],
+    queryFn: getPopularProducts,
+  })
+
   return (
     <Card className="col-span-3">
       <CardHeader className=" pb-8">
@@ -32,7 +39,65 @@ export function PopularProductsChart() {
         </div>
       </CardHeader>
       <CardContent>
-        <ResponsiveContainer width="100%" height={240}>
+        {popularProducts && (
+          <ResponsiveContainer width="100%" height={240}>
+            <PieChart style={{ fontSize: 12 }}>
+              <Pie
+                data={popularProducts}
+                nameKey="product"
+                dataKey="amount"
+                cx="50%"
+                cy="50%"
+                outerRadius={86}
+                innerRadius={64}
+                strokeWidth={8}
+                labelLine={false}
+                label={({
+                  cx,
+                  cy,
+                  midAngle,
+                  innerRadius,
+                  outerRadius,
+                  value,
+                  index,
+                }) => {
+                  const RADIAN = Math.PI / 180
+                  const radius = 12 + innerRadius + (outerRadius - innerRadius)
+                  const x = cx + radius * Math.cos(-midAngle * RADIAN)
+                  const y = cy + radius * Math.sin(-midAngle * RADIAN)
+
+                  return (
+                    <text
+                      x={x}
+                      y={y}
+                      className="fill-muted-foreground text-xs"
+                      textAnchor={x > cx ? 'start' : 'end'}
+                      dominantBaseline="central"
+                    >
+                      {popularProducts[index].product.length > 12
+                        ? popularProducts[index].product
+                            .substring(0, 12)
+                            .concat('...')
+                        : popularProducts[index].product}{' '}
+                      ({value})
+                    </text>
+                  )
+                }}
+              >
+                {popularProducts.map((_, index) => {
+                  return (
+                    <Cell
+                      key={`cell-${index}`}
+                      fill={COLORS[index]}
+                      className="stroke-background hover:opacity-80"
+                    />
+                  )
+                })}
+              </Pie>
+            </PieChart>
+          </ResponsiveContainer>
+        )}
+        {/* <ResponsiveContainer width="100%" height={240}>
           <PieChart style={{ fontSize: 12 }}>
             <Pie
               data={data}
@@ -83,7 +148,7 @@ export function PopularProductsChart() {
               ))}
             </Pie>
           </PieChart>
-        </ResponsiveContainer>
+        </ResponsiveContainer> */}
       </CardContent>
     </Card>
   )
--- pizzashop-web/src/services/get-day-orders-amount.ts
+++ pizzashop-web/src/services/get-day-orders-amount.ts
@@ -7,7 +7,7 @@ export interface GetDayOrdersAmountResponse {
 
 export async function getDayOrdersAmount() {
   const response = await api.get<GetDayOrdersAmountResponse>(
-    '/metrics/day-orders-amount  ',
+    '/metrics/day-orders-amount',
   )
 
   return response.data
